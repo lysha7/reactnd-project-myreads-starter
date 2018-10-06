d7319f6234fbeef2f6c5f7aa66345853bd4c5c70
Alysha Gil <alyshanicole7@gmail.com>
2018-10-06 18:12:50 -0400

    Add search functionality to search page

--- src/RenderBooks.js
+++ src/RenderBooks.js
@@ -5,23 +5,26 @@ class RenderBooks extends React.Component {
 
 	render() {
 		const { books } = this.props
-		console.log(books)
-
+		if (books !== undefined && books.length !== 0) {
+			return(
+		        <ol className="books-grid">
+		        	{books.map((book) => (
+						<li key={book.id}>
+						<div className="book">
+						  <div className="book-top">
+						    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+						    <BookshelfChanger/>
+						  </div>
+						  <div className="book-title">{book.title !== undefined && book.title}</div>
+						  <div className="book-authors">{book.authors !== undefined && book.authors.join(', ')}</div>
+						</div>
+						</li>
+		        	))}
+		        </ol>
+			)
+		}
 		return(
-	        <ol className="books-grid">
-	        	{books.map((book) => (
-					<li key={book.id}>
-					<div className="book">
-					  <div className="book-top">
-					    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-					    <BookshelfChanger/>
-					  </div>
-					  <div className="book-title">{book.title}</div>
-					  <div className="book-authors">{book.authors.join(', ')}</div>
-					</div>
-					</li>
-	        	))}
-	        </ol>
+			<p>There are no books to display</p>
 		)
 	}
 }
