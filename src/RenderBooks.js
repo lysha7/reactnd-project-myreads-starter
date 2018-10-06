e8e93164e9787412a711a35b4d7e00e13af14a14
Alysha Gil <alyshanicole7@gmail.com>
2018-10-06 12:48:47 -0400

    Get books in Search to appear from API

--- src/RenderBooks.js
+++ src/RenderBooks.js
@@ -4,18 +4,23 @@ import BookshelfChanger from './BookshelfChanger'
 class RenderBooks extends React.Component {
 
 	render() {
+		const { books } = this.props
+		console.log(books)
+
 		return(
 	        <ol className="books-grid">
-	          <li>
-	            <div className="book">
-	              <div className="book-top">
-	                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-	                <BookshelfChanger/>
-	              </div>
-	              <div className="book-title">To Kill a Mockingbird</div>
-	              <div className="book-authors">Harper Lee</div>
-	            </div>
-	          </li>
+	        	{books.map((book) => (
+					<li key={book.id}>
+					<div className="book">
+					  <div className="book-top">
+					    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+					    <BookshelfChanger/>
+					  </div>
+					  <div className="book-title">{book.title}</div>
+					  <div className="book-authors">{book.authors.join(', ')}</div>
+					</div>
+					</li>
+	        	))}
 	        </ol>
 		)
 	}
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,31 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import RenderBooks from './RenderBooks'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends React.Component {
 
+	state = {
+		books: []
+	}
+
+	componentDidMount() {
+		BooksAPI.getAll().then((books) => {
+			this.setState({ books })
+		})
+	}
+
 	render() {
 		return (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link to="/" className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+          	<div className="search-books">
+            	<div className="search-books-bar">
+              		<Link to="/" className="close-search">Close</Link>
+              		<div className="search-books-input-wrapper">
+		                {/*
+		                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+		                  You can find these search terms here:
+		                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+		                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+		                  you don't find a specific author or title. Every search is limited by search terms.
+		                */}
+                		<input type="text" placeholder="Search by title or author"/>
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <RenderBooks/>
-            </div>
-          </div>
+              		</div>
+            	</div>
+            	<div className="search-books-results">
+              		<RenderBooks
+              			books={this.state.books}
+              		/>
+            	</div>
+          	</div>
 		)
 	}
 }
